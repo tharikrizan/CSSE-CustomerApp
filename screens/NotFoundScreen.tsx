2f08d7a3d89b0e3bc48c28c57c1dd2f98fcf0971
tharikrizan <tharik.r@yahoo.com>
2020-10-14 07:33:59 +0530

    first commit

--- screens/NotFoundScreen.tsx
+++ screens/NotFoundScreen.tsx
@@ -11,7 +11,7 @@ export default function NotFoundScreen({
     <View style={styles.container}>
       <Text style={styles.title}>This screen doesn't exist.</Text>
       <TouchableOpacity onPress={() => navigation.replace('Root')} style={styles.link}>
-        <Text style={styles.linkText}>Go to home screen!</Text>
+        <Text style={styles.linkText}>Go to home screen 404!</Text>
       </TouchableOpacity>
     </View>
   );
